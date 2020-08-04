3ef327d632323440b41138d47f40d045c1f31dd9
Cathrine Kanini <catherinekanini001@gmail.com>
2020-08-04 22:09:18 +0300

    js file updated

--- JS/scripts.js
+++ JS/scripts.js
@@ -36,7 +36,7 @@ function Getpizza( name,size,crust,topping, total ){
 }
 $(document).ready(function(){
   $("button.proceed").click(function(event){
-   let pname = $(".name option:selected").val();
+  //  let pname = $(".name option:selected").val();
    let psize = $("#size option:selected").val();
    let pcrust = $("#crust option:selected").val();
    let ptopping = [];
@@ -70,13 +70,13 @@ $(document).ready(function(){
           crust_price = 0;
         break;
         case "Crispy":
-          crust_price = 200;
+          crust_price = 150;
         break;
         case "Stuffed":
-          crust_price = 250;
+          crust_price = 100;
         break;
         case "Gluten-free":
-          crust_price = 180;
+          crust_price = 200;
         break;
         default:
           console.log("No price"); 
@@ -91,11 +91,11 @@ $(document).ready(function(){
         $("div.choice").hide();
         alert("Hungry peep please select pizza size and crust"); 
       }
-      else{
-        $("button.proceed").hide();
-        $("#info").hide();
-        $("div.choice").slideDown(1000);
-      }
+      // else{
+      //   $("button.proceed").hide();
+      //   $("#info").hide();
+      //   $("div.choice").slideDown(1000);
+      // }
   
       total = price + crust_price + topping_value;
       console.log(total);
@@ -103,11 +103,11 @@ $(document).ready(function(){
       checkoutTotal = checkoutTotal + total;
 
 
-      $("button#checkout").click(function(){ 
-         $("button#checkout").hide();
-         $("button.addPizza").hide();
-         $("button.deliver").slideDown(1000);
-         $("#addedprice").slideDown(1000);
+      $("#checkout").click(function(){ 
+        //  $("button#checkout").hide();
+        //  $("button.addPizza").hide();
+        //  $("button.deliver").slideDown(1000);
+        //  $("#addedprice").slideDown(1000);
          console.log("Your total bills is sh. "+checkoutTotal);
          $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
        });
